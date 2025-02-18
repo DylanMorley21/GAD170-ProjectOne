a56a70b5c87b63142ba09d93688392b287b1c9b1
DylanMorleyStudentSAE <1034172@student.sae.edu.au>
2025-02-19 00:09:24 +1000

    Project work.

--- Assets/Scripts/Project One.cs
+++ Assets/Scripts/Project One.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ProjectOne : MonoBehaviour
 {
+    int playerHealth = 0;
+    int playerVitality = 0;
+    int playerStrength = 0;
+    int playerDexterity = 0;
+    int playerPowerLevel = 0;
+
+    int skeletonHealth = 0;
+    int skeletonVitality = 0;
+    int skeletonStrength = 0; 
+    int skeletonDexterity = 0;
+    int skeletonPowerLevel = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,6 @@ public class ProjectOne : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
